45a552fdd536a6f6b9f66c10d176003666aa7824
Umair Khan <103140938+bossumair@users.noreply.github.com>
2022-10-02 20:56:57 +0500

    assign

---
+++ Lab Assignment 1/prac.js
@@ -0,0 +1,7 @@
+/**Array Methods
+ * Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ */
+/*length property* */
+let arr1 = [1, 2, 3, 4, 5];
+console.log(arr1[arr1.length - 1]);
+/**Push and pop */
